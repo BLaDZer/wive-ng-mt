b1c934f1cfc4ca85425dde62a70bcba556ac65ac
Evgeniy Manachkin <sfstudio@mail.ru>
2014-11-26 10:01:12 +0600

    increase apply and reboot timeout

--- user/goahead/cgi-src/reboot.sh
+++ user/goahead/cgi-src/reboot.sh
@@ -4,7 +4,7 @@ echo "Content-type: text/html"
 echo ''
 echo '<html>'
 echo '<head><script type="text/javascript" src="/js/ajax.js"></script></head>'
-echo '<body><script lang=\"JavaScript\">ajaxReloadDelayedPage(40000);</script></body>'
+echo '<body><script lang=\"JavaScript\">ajaxReloadDelayedPage(60000);</script></body>'
 echo '</html>'
 
 fs backup_nvram 		> /dev/console
