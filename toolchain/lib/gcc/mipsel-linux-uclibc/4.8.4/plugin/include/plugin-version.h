6827183389516f38c45841338ca28efb3d38c4cf
Evgeniy Manachkin <sfstudio@mail.ru>
2015-01-14 23:01:27 +0600

    toolchain: update kernel headers and gcc up to 4.8.4 release.

--- toolchain/lib/gcc/mipsel-linux-uclibc/4.8.4/plugin/include/plugin-version.h
+++ toolchain/lib/gcc/mipsel-linux-uclibc/4.8.4/plugin/include/plugin-version.h
@@ -6,8 +6,8 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "4.8.4";
-static char datestamp[] = "20141211";
-static char devphase[] = "prerelease";
+static char datestamp[] = "20141219";
+static char devphase[] = "";
 static char revision[] = "";
 
 /* FIXME plugins: We should make the version information more precise.
