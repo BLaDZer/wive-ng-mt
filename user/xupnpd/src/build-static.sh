0f23dd8f2addbc93e3a74dc691ff4286e75dcad2
Evgeniy Manachkin <sfstudio@mail.ru>
2015-01-26 07:51:12 +0600

    xupnpd up to 404.

--- user/xupnpd/src/build-static.sh
+++ user/xupnpd/src/build-static.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-make clean; make STATIC=true bcm947xx_ddwrt
 
-make clean; make STATIC=true bcm947xx
+make clean; make STATIC=true bcm947x_ddwrt
 
-make clean; make STATIC=true ar71xx
+make clean; make STATIC=true bcm947x_backfire
+
+make clean; make STATIC=true ar71xx_backfire
+
+make clean; make STATIC=true ar231x_backfire
+
+make clean; make STATIC=true bcm947x_backfire
+
+make clean; make STATIC=true bcm2708
+
+make clean; make STATIC=true bcm47xx
+
+make clean; make STATIC=true bcm63xx
+
+make clean; make STATIC=true ar7xxx
 
 make clean; make STATIC=true ar231x
 
--- user/xupnpd/src/build.sh
+++ user/xupnpd/src/build.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-make clean; make bcm947xx_ddwrt
 
-make clean; make bcm947xx
+make clean; make bcm947x_ddwrt
 
-make clean; make ar71xx
+make clean; make bcm947x_backfire
+
+make clean; make ar71xx_backfire
+
+make clean; make ar231x_backfire
+
+make clean; make bcm947x_backfire
+
+make clean; make bcm2708
+
+make clean; make bcm47xx
+
+make clean; make bcm63xx
+
+make clean; make ar7xxx
 
 make clean; make ar231x
 
