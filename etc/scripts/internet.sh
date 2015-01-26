270b6e16957fb1eb7a8ca13d9c2bf645249d24a5
Evgeniy Manachkin <sfstudio@mail.ru>
2015-01-26 10:43:23 +0600

    internet.sh: fix log message.

--- etc/scripts/internet.sh
+++ etc/scripts/internet.sh
@@ -127,10 +127,11 @@ apcli_config() {
 	# delete ra0 from bridge and down if only apcli-bridge
 	eval `nvram_buf_get 2860 ApCliClientOnly`
 	if  [ "$ApCliClientOnly" = "1" ] && [ "$OperationMode" = "0" -o "$OperationMode" = "3" ]; then
-    	    echo "APCLI Only client mode enable, shutdown AP interface."
 	    if [ "$first_wlan_apcli" != "" ] && [ "$first_wlan_apcli" = "$ApCliIfName" ]; then
+    		echo "APCLI Only client mode enable, shutdown AP interface $first_wlan_root_if."
     		delif_from_br $first_wlan_root_if
     	    elif [ "$second_wlan_apcli" != "" ] && [ "$second_wlan_apcli" = "$ApCliIfName" ]; then
+    		echo "APCLI Only client mode enable, shutdown AP interface $second_wlan_root_if."
     		delif_from_br $second_wlan_root_if
     	    fi
 	fi
