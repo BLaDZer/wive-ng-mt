b4468f90542042f3ce8e0a9880e6f3730b86b371
Evgeniy Manachkin <sfstudio@mail.ru>
2015-03-06 18:49:51 +0600

    goahead: add correct platform name for 7620+7612 devices.

--- user/goahead/src/utils.c
+++ user/goahead/src/utils.c
@@ -658,6 +658,8 @@ static int getPlatform(int eid, webs_t wp, int argc, char_t **argv)
 #if defined(CONFIG_RALINK_MT7620) && defined(CONFIG_RAETH_ESW)
 #if defined(CONFIG_MT7610_AP) || defined(CONFIG_MT7610_AP_MODULE)
     return websWrite(wp, T("MT7620 2T2R 2.4GHz, MT7610 1T1R 5GHz, 100FDX"));
+#elif defined(CONFIG_MT76X2_AP) || defined(CONFIG_MT76X2_AP_MODULE)
+    return websWrite(wp, T("MT7620 2T2R 2.4GHz, MT7612 2T2R 5GHz, 100FDX"));
 #else
     return websWrite(wp, T("MT7620 2T2R 2.4GHz, 100FDX"));
 #endif
