1dfb7ff8219503c9bf0b51aaf2837758a1f958b7
Evgeniy Manachkin <sfstudio@mail.ru>
2015-03-22 23:47:31 +0600

    xupnpd: use mpeg as default mime type (more devices compat whith this type).

--- user/xupnpd/src/xupnpd.lua
+++ user/xupnpd/src/xupnpd.lua
@@ -72,7 +72,7 @@ cfg.cache_size=4
 cfg.cache_ttl=300
 
 -- default mime type (mpeg, mpeg_ts, mpeg1, mpeg2, ts, ...)
-cfg.default_mime_type='mpeg_ts'
+cfg.default_mime_type='mpeg'
 
 -- feeds update interval (seconds, 0 - disabled)
 cfg.feeds_update_interval=0
