cbf5a46f3f873114725f83437c2114630d9f040b
Evgeniy Manachkin <sfstudio@mail.ru>
2015-04-09 02:22:47 +0600

    toolchain: update gcc to 07.04.2015 4.8.5 version form git, update headers.

--- toolchain/usr/include/bits/uClibc_config.h
+++ toolchain/usr/include/bits/uClibc_config.h
@@ -7,7 +7,7 @@
 #define __UCLIBC_SUBLEVEL__ 33
 /* Automatically generated make config: don't edit */
 /* Version: 0.9.33.2 */
-/* Sun Mar 15 13:37:50 2015 */
+/* Thu Apr  9 01:48:43 2015 */
 #undef __TARGET_alpha__
 #undef __TARGET_arm__
 #undef __TARGET_avr32__
