d93e98857434225d9507c1094a8432e5576f0a76
Evgeniy Manachkin <sfstudio@mail.ru>
2015-04-20 21:08:02 +0600

    REBUILD 2.9.4: init.d: services_restart.sh: reconfigure and start radvd/dhcp6s at apply in misc

--- etc/scripts/services_restart.sh
+++ etc/scripts/services_restart.sh
@@ -43,9 +43,12 @@ fi
 
 ##########################################################
 # reconfigure ipv6 tunnels after wan/vpn adress renew
+# and reconfigure and start radvd/dhcp6s at apply in misc
 ##########################################################
 if [ "$IPv6OpMode" = "2" -o "$IPv6OpMode" = "3" ]  && [ "$MODE" = "dhcp" -o "$MODE" = "ppp" ]; then
     service six restart
+elif [ "$MODE" = "misc" ]; then
+    service six dhcpradvdreconf
 fi
 
 ##########################################################
