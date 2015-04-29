21c21e7a421bf769f5a6d125950343561a782f01
Evgeniy Manachkin <sfstudio@mail.ru>
2015-04-29 16:26:06 +0600

    raeth: add support TX easy flow control for MT7621/MT7530 GSW, show flow control and EEE status and support JF control on MT7621/MT7530 GSW

--- linux-3.4.x/drivers/net/raeth/ra_esw_ioctl.c
+++ linux-3.4.x/drivers/net/raeth/ra_esw_ioctl.c
@@ -1134,7 +1134,6 @@ static void esw_storm_control(u32 port_id, int set_bcast, int set_mcast, int set
 
 static void esw_jumbo_control(u32 jumbo_frames_enabled)
 {
-#if !defined (CONFIG_MT7530_GSW)
 	u32 reg_gmaccr;
 
 	reg_gmaccr = esw_reg_get(REG_ESW_MAC_GMACCR);
@@ -1148,9 +1147,6 @@ static void esw_jumbo_control(u32 jumbo_frames_enabled)
 	}
 
 	esw_reg_set(REG_ESW_MAC_GMACCR, reg_gmaccr);
-#else
-	// todo (mt7530 documentation needed)
-#endif
 }
 
 static void esw_eee_control(u32 green_ethernet_enabled)
@@ -1248,9 +1244,8 @@ static u32 esw_status_link_port(u32 port_id)
 
 static u32 esw_status_speed_port(u32 port_id)
 {
-	u32 port_link;
-	u32 port_duplex;
-	u32 port_speed;
+	u32 port_link, port_duplex, port_speed;
+	u32 port_eee, port_fc_rx, port_fc_tx;
 	u32 reg_pmsr;
 
 #if defined (CONFIG_GE2_INTERNAL_GPHY_P0) || defined (CONFIG_GE2_INTERNAL_GPHY_P4)
@@ -1271,8 +1266,11 @@ static u32 esw_status_speed_port(u32 port_id)
 
 	port_duplex = (reg_pmsr >> 1) & 0x1;
 	port_speed  = (reg_pmsr >> 2) & 0x3;
+	port_fc_tx  = (reg_pmsr >> 4) & 0x1;
+	port_fc_rx  = (reg_pmsr >> 5) & 0x1;
+	port_eee    = (reg_pmsr >> 6) & 0x3;
 
-	return ((port_link << 16) | (port_duplex << 8) | port_speed);
+	return ((port_link << 16) | (port_eee << 11) | (port_fc_rx << 10) | (port_fc_tx << 9) | (port_duplex << 8) | port_speed);
 }
 
 static u32 esw_status_link_ports(int is_wan_ports)
@@ -1598,9 +1596,10 @@ static void change_port_link_mode(u32 phy_port_id, u32 port_link_mode)
 
 	switch (i_port_flowc)
 	{
+	case SWAPI_LINK_FLOW_CONTROL_TX_ASYNC:
 	case SWAPI_LINK_FLOW_CONTROL_DISABLE:
-		/* disable pause support (A5) */
-		esw_phy_ana &= ~(1<<10);
+		/* disable pause ability (A6,A5) */
+		esw_phy_ana &= ~((1<<11)|(1<<10));
 		flow_desc = "OFF";
 		break;
 	}
@@ -1640,6 +1639,14 @@ static void change_port_link_mode(u32 phy_port_id, u32 port_link_mode)
 			/* disable ability 100 FD, 100 HD, 10 FD, 10 HD */
 			esw_phy_ana &= ~((1<<8)|(1<<7)|(1<<6)|(1<<5));
 			link_desc = "1000FD [AN]";
+		}
+		
+		/* set auto-negotiation advertisement register [11] */
+		if (i_port_speed <= SWAPI_LINK_SPEED_MODE_AUTO_1000_FD &&
+		    i_port_flowc == SWAPI_LINK_FLOW_CONTROL_TX_ASYNC) {
+			/* enable asymmetric pause ability (A6) */
+			esw_phy_ana |= (1<<11);
+			flow_desc = "TX Asy";
 		}
 		
 		/* set 1000Base-T control register [8,9] */
