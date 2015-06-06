b47cec9fb9205da39e94177293975833c33fec99
Evgeniy Manachkin <sfstudio@mail.ru>
2015-06-07 01:35:41 +0600

    Rebuild 3.1.2: toolchain: update gcc to 04.06.2015 4.8.5 version form git, update headers.

--- toolchain/usr/include/bits/uClibc_config.h
+++ toolchain/usr/include/bits/uClibc_config.h
@@ -7,7 +7,7 @@
 #define __UCLIBC_SUBLEVEL__ 33
 /* Automatically generated make config: don't edit */
 /* Version: 0.9.33.2 */
-/* Tue May 26 10:35:09 2015 */
+/* Sun Jun  7 01:09:28 2015 */
 #undef __TARGET_alpha__
 #undef __TARGET_arm__
 #undef __TARGET_avr32__
