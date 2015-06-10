c8f537a50be1d5c10d0868c325c77a5a9b07f99c
Evgeniy Manachkin <sfstudio@mail.ru>
2015-06-10 20:26:26 +0600

    goahead: fix drop security settings after mode change.

--- user/goahead/src/wireless.c
+++ user/goahead/src/wireless.c
@@ -688,45 +688,6 @@ const mbss_param_t mbss_params[] =
 	{ NULL, 0 }
 };
 
-static void revise_mbss_updated_values(webs_t wp, int bssnum)
-{
-	const mbss_param_t *parm = mbss_params;
-	char tempbuf[32], tmpvalue[128];
-	int i;
-
-	while (parm->name != NULL)
-	{
-		if (parm->is_comp)
-		{
-			tmpvalue[0] = '\0';
-			for (i=0; i<bssnum; i++) /* Build string from parameters */
-			{
-				catIndex(tempbuf, parm->name, (i+1));
-				char *v = websGetVar(wp, tempbuf, T(""));
-				if (v == NULL)
-					v = "";
-				if (bssnum > 1)
-				    sprintf(tmpvalue, "%s%s;", tmpvalue, v); /* multiple ssid by ; as separator */
-				else
-				    sprintf(tmpvalue, "%s%s", tmpvalue, v); /* separator ; not need if only one ssid enabled */
-			}
-			nvram_bufset(RT2860_NVRAM, parm->name, tmpvalue);
-		}
-		else
-		{
-			for (i=0; i<bssnum; i++) /* set-up parameter by parameter */
-			{
-				catIndex(tempbuf, parm->name, (i+1));
-				char *v = websGetVar(wp, tempbuf, T(""));
-				if (v == NULL)
-					v = "";
-				nvram_bufset(RT2860_NVRAM, tempbuf, v);
-			}
-		}
-		parm++;
-	}
-}
-
 /* goform/wirelessBasic */
 static void wirelessBasic(webs_t wp, char_t *path, char_t *query)
 {
@@ -996,8 +957,6 @@ static void wirelessBasic(webs_t wp, char_t *path, char_t *query)
 #ifndef CONFIG_RT_SECOND_IF_NONE
 	nvram_bufset(RT2860_NVRAM, "RadioOffINIC", (web_radio_ac_on) ? "0" : "1");
 #endif
-	// update mbss parametrs
-	revise_mbss_updated_values(wp, new_bssid_num);
 
 	nvram_commit(RT2860_NVRAM);
 	nvram_close(RT2860_NVRAM);
