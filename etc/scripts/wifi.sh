755971468693c4f4214965bd1f66c5ac004ff25e
Evgeniy Manachkin <sfstudio@mail.ru>
2015-06-13 03:52:38 +0600

    wifi.sh: cleanup: remove unused and set by profile options.

--- etc/scripts/wifi.sh
+++ etc/scripts/wifi.sh
@@ -14,8 +14,7 @@ echo ">>>>> RECONFIGURE WIFI IF = $1 <<<<<<<<<<"
 ######################################################################################################
 eval `nvram_buf_get 2860 OperationMode RadioOff RadioOffINIC \
 	Channel ChannelINIC AutoChannelSelect AutoChannelSelectINIC \
-	AutoConnect GreenAP HT_OpMode DyncVgaEnable MO_FalseCCATh MO_LowFalseCCATh \
-	AP2040Rescan HT_BSSCoexistence McastPhyMode McastMcs M2UEnabled`
+	AutoConnect DyncVgaEnable MO_FalseCCATh MO_LowFalseCCATh M2UEnabled`
 ########################################STAMODE param#################################################
 if [ "$OperationMode" = "2" ]; then
     if [ "$AutoConnect" != "" ]; then
@@ -42,15 +41,6 @@ if [ "$2" != "5GHZ" ]; then
     iwpriv $1 set ShortRetry=3
 fi
 ########################################MULTICAST param##############################################
-if [ "$CONFIG_RT2860V2_AP_MCAST_RATE_SPECIFIC" != "" ]; then
-    # always set mcast mode if enabled
-    if [ "$McastPhyMode" != "" ]; then
-        iwpriv "$1" set McastPhyMode="$McastPhyMode"
-    fi
-    if [ "$McastMcs" != "" ]; then
-        iwpriv "$1" set McastMcs="$McastMcs"
-    fi
-fi
 if [ "$CONFIG_RT2860V2_AP_IGMP_SNOOP" != "" ]; then
     # in bridged mode direct enable Multicast2Unicast in wifi drivers if enabled
     # in others modes auto enable by igmpproxy
@@ -60,14 +50,6 @@ if [ "$CONFIG_RT2860V2_AP_IGMP_SNOOP" != "" ]; then
 	else
 	    iwpriv "$1" set IgmpSnEnable=0
 	fi
-    fi
-fi
-########################################GREEN mode####################################################
-if [ "$CONFIG_RT2860V2_AP_GREENAP" != "" ] && [ "$2" != "5GHZ" ]; then
-    if [ "$HT_OpMode" = "1" ] || [ "$GreenAP" = "1" ]; then
-	iwpriv "$1" set GreenAP=1
-    else
-	iwpriv "$1" set GreenAP=0
     fi
 fi
 #####################################Reduce interference##############################################
@@ -88,19 +70,12 @@ if [ "$AutoChannelSelect" = "1" -a "$2" != "5GHZ" ] || \
     # rescan and select optimal channel
     # first need scan
     iwpriv "$1" set SiteSurvey=1
-    # second select channel
+    # second reselect channel
     iwpriv "$1" set AutoChannelSel=1
 else
     if [ "$2" = "5GHZ" ]; then
 	iwpriv "$1" set Channel="$ChannelINIC"
     else
 	iwpriv "$1" set Channel="$Channel"
-    fi
-fi
-###########################################ALWAYS END#################################################
-# rescan coexist mode
-if [ "$CONFIG_RT2860V2_AP_80211N_DRAFT3" != "" ]; then
-    if [ "$AP2040Rescan" = "1" ] && [ "$HT_BSSCoexistence" = "1" ]; then
-	iwpriv "$1" set AP2040Rescan=1
     fi
 fi
