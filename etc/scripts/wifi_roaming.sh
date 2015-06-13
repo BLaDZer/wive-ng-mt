c62d0e05c1fdf6b647033dc7321d142e423fe4fc
Evgeniy Manachkin <sfstudio@mail.ru>
2015-06-13 07:14:36 +0600

    init.d: add basic fast roaming parametrs tune support for 76x2 drivers.

--- etc/scripts/wifi_roaming.sh
+++ etc/scripts/wifi_roaming.sh
@@ -3,7 +3,15 @@
 # include kernel config
 . /etc/scripts/config.sh
 
-###################################PARAMETRS TABLE#################################################################
+is_up=`ip link show $1 up`
+if [ "$is_up" = "" ]; then
+    echo ">>>>> Wifi interface $1 not up - skip roaming tune <<<<<<<<<<"
+    exit 0
+fi
+
+echo ">>>>> RECONFIGURE ROAMING WIFI IF = $1 <<<<<<<<<<"
+
+###############################################PARAMETRS TABLE##################################################################
 # ApProbeRspTimes	- range 0 - 10 times, limit probe reqest per client, default 3
 # AuthRspFail		- range 0 - -100 dBm, reject auth req due to weak signal, default 0 (off)
 # AuthRspRssi		- range 0 - -100 dBm, ignore auth req due to weak signal, default 0 (off)
@@ -11,36 +19,25 @@
 # AssocRspIgnor		- range 0 - -100 dBm, ignore assoc req due to weak signal, default 0 (off)
 # KickStaRssiLow	- range 0 - -100 dBm, auto disonnect sta if rssi low, default 0 (off)
 # ProbeRspRssi		- range 0 - -100 dBm, auto disonnect sta if rssi provw requests low, default 0 (off)
-###################################################################################################################
-eval `nvram_buf_get 2860 OperationMode FastRoaming \
-			 ApProbeRspTimes AuthRspFail AuthRspRssi AssocReqRssiThres AssocRspIgnor \
-			 KickStaRssiLow ProbeRspRssi`
-
-is_up=`ip link show $1 up`
-if [ "$is_up" = "" ] || [ "$FastRoaming" != "1" ]; then
-    exit 0
+################################################################################################################################
+eval `nvram_buf_get 2860 ApProbeRspTimes AuthRspFail AuthRspRssi AssocReqRssiThres AssocRspIgnor KickStaRssiLow ProbeRspRssi`
+################################################################################################################################
+if [ "$ApProbeRspTimes" != "" ]; then
+    iwpriv "$1" set ApProbeRspTimes="$ApProbeRspTimes"
 fi
-
-echo ">>>>> RECONFIGURE ROAMING WIFI IF = $1 <<<<<<<<<<"
-###################################################################################################################
-if [ "$OperationMode" = "0" ] || [ "$OperationMode" = "1" ] || [ "$OperationMode" = "3" ]; then
-    if [ "$ApProbeRspTimes" != "" ]; then
-	iwpriv "$1" set ApProbeRspTimes="$ApProbeRspTimes"
-    fi
-    if [ "$AuthRspFail" != "" ]; then
-	iwpriv "$1" set AuthRspFail="$AuthRspFail"
-    fi
-    if [ "$AssocReqRssiThres" != "" ]; then
-	iwpriv "$1" set AssocReqRssiThres="$AssocReqRssiThres"
-    fi
-    if [ "$AssocRspIgnor" != "" ]; then
-	iwpriv "$1" set AssocRspIgnor="$AssocRspIgnor"
-    fi
-    if [ "$KickStaRssiLow" != "" ]; then
-	iwpriv "$1" set KickStaRssiLow="$KickStaRssiLow"
-    fi
-    if [ "$ProbeRspRssi" != "" ]; then
-	iwpriv "$1" set ProbeRspRssi="$ProbeRspRssi"
-    fi
+if [ "$AuthRspFail" != "" ]; then
+    iwpriv "$1" set AuthRspFail="$AuthRspFail"
 fi
-######################################################################################################
+if [ "$AssocReqRssiThres" != "" ]; then
+    iwpriv "$1" set AssocReqRssiThres="$AssocReqRssiThres"
+fi
+if [ "$AssocRspIgnor" != "" ]; then
+    iwpriv "$1" set AssocRspIgnor="$AssocRspIgnor"
+fi
+if [ "$KickStaRssiLow" != "" ]; then
+    iwpriv "$1" set KickStaRssiLow="$KickStaRssiLow"
+fi
+if [ "$ProbeRspRssi" != "" ]; then
+    iwpriv "$1" set ProbeRspRssi="$ProbeRspRssi"
+fi
+################################################################################################################################
