291ade14d10707d2f0c667020a5f75ac59ab4e11
Evgeniy Manachkin <sfstudio@mail.ru>
2015-06-23 16:15:40 +0600

    toolchain: update gcc to 20150619 4.8.5-RC version form git (rebuild after host update to Mageia 5).

--- toolchain/lib/gcc/mipsel-linux-uclibc/4.8.5/include/c++/mipsel-linux-uclibc/bits/c++config.h
+++ toolchain/lib/gcc/mipsel-linux-uclibc/4.8.5/include/c++/mipsel-linux-uclibc/bits/c++config.h
@@ -31,7 +31,7 @@
 #define _GLIBCXX_CXX_CONFIG_H 1
 
 // The current version of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20150614
+#define __GLIBCXX__ 20150619
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
