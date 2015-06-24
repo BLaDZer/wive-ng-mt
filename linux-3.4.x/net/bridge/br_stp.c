de0556f47f312f0ac5184dad86a6d8bc76c50506
Evgeniy Manachkin <sfstudio@mail.ru>
2015-06-24 14:50:25 +0600

    kernel: bridge: multicast: start querier timer when running user-space stp.

--- linux-3.4.x/net/bridge/br_stp.c
+++ linux-3.4.x/net/bridge/br_stp.c
@@ -385,7 +385,6 @@ static void br_make_forwarding(struct net_bridge_port *p)
 	else
 		p->state = BR_STATE_LEARNING;
 
-	br_multicast_enable_port(p);
 	br_log_state(p);
 	br_ifinfo_notify(RTM_NEWLINK, p);
 
@@ -419,6 +418,8 @@ void br_port_state_selection(struct net_bridge *br)
 			}
 		}
 
+		if (p->state != BR_STATE_BLOCKING)
+			br_multicast_enable_port(p);
 		if (p->state == BR_STATE_FORWARDING)
 			++liveports;
 	}
