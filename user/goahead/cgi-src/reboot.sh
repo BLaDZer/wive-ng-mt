6199d871e94244f6257f8ce7870aa7562a15232c
Evgeniy Manachkin <sfstudio@mail.ru>
2015-06-26 15:36:31 +0600

    goahead: send SIGUSR2 to udhcpc for ip release before reboot.

--- user/goahead/cgi-src/reboot.sh
+++ user/goahead/cgi-src/reboot.sh
@@ -7,7 +7,11 @@ echo '<head><script type="text/javascript" src="/js/ajax.js"></script></head>'
 echo '<body><script lang=\"JavaScript\">ajaxReloadDelayedPage(60000);</script></body>'
 echo '</html>'
 
+# send release to dhcp server
+killall -q -SIGUSR2 udhcpc	> /dev/null 2>&1
+# save rwfs
 fs save				> /dev/console 2>&1
+# unload all services
 /etc/scripts/wifi_unload.sh     > /dev/console 2>&1
 
 reboot
