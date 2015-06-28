3e497f0e84f4863191814eaa2a50b2eaea3a56ee
Evgeniy Manachkin <sfstudio@mail.ru>
2015-06-28 20:38:11 +0600

    increase save and reboot reload page timeout.

--- user/goahead/cgi-src/reboot.sh
+++ user/goahead/cgi-src/reboot.sh
@@ -4,7 +4,7 @@ echo "Content-type: text/html"
 echo ''
 echo '<html>'
 echo '<head><script type="text/javascript" src="/js/ajax.js"></script></head>'
-echo '<body><script lang=\"JavaScript\">ajaxReloadDelayedPage(60000);</script></body>'
+echo '<body><script lang=\"JavaScript\">ajaxReloadDelayedPage(80000);</script></body>'
 echo '</html>'
 
 # send release to dhcp server
