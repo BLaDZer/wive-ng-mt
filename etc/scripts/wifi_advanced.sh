ff1551e0436d8060c0982a798a04b8ab4e76bdd3
Evgeniy Manachkin <sfstudio@mail.ru>
2015-06-28 16:18:45 +0600

    wifi_advanced.sh remove not more needed hack.

--- etc/scripts/wifi_advanced.sh
+++ etc/scripts/wifi_advanced.sh
@@ -11,10 +11,9 @@ fi
 
 echo ">>>>> RECONFIGURE WIFI IF = $1 <<<<<<<<<<"
 
-######################################################################################################
-eval `nvram_buf_get 2860 OperationMode RadioOff RadioOffINIC \
-			    AutoConnect DyncVgaEnable M2UEnabled RateAlg`
-########################################STAMODE param#################################################
+##########################################################################################################
+eval `nvram_buf_get 2860 OperationMode RadioOff RadioOffINIC AutoConnect DyncVgaEnable M2UEnabled RateAlg`
+########################################STAMODE param#####################################################
 if [ "$OperationMode" = "2" ]; then
     if [ "$AutoConnect" != "" ]; then
 	iwpriv "$1" set AutoReconnect="$AutoConnect"
@@ -22,14 +21,14 @@ if [ "$OperationMode" = "2" ]; then
   # in sta mode exit
   exit 0
 fi
-########################################APMODE param#################################################
-#########################################ON/OFF param################################################
+########################################APMODE param#####################################################
+#########################################ON/OFF param####################################################
 if [ "$RadioOff" = "1" -a "$2" != "5GHZ" ] || [ "$RadioOffINIC" = "1" -a "$2" = "5GHZ" ]; then
     iwpriv "$1" set RadioOn=0
     echo ">>>> WIFI $1 DISABLED <<<<"
     exit 0
 fi
-########################################MULTICAST PARAMS###############################################
+########################################MULTICAST PARAMS#################################################
 if [ "$CONFIG_RT2860V2_AP_IGMP_SNOOP" != "" ]; then
     # in bridged mode direct enable Multicast2Unicast in wifi drivers if enabled
     # in others modes auto enable by igmpproxy
@@ -41,17 +40,9 @@ if [ "$CONFIG_RT2860V2_AP_IGMP_SNOOP" != "" ]; then
 	fi
     fi
 fi
-#####################################DYNAMIC VARIABLE GAIN############################################
+#####################################DYNAMIC VARIABLE GAIN###############################################
 if [ "$DyncVgaEnable" = "1" ]; then
     iwpriv "$1" set DyncVgaEnable=1
 else
     iwpriv "$1" set DyncVgaEnable=0
-fi
-################WORKAROUND FOR TX RING FULL IN WIFI DRIVERS (ONLY 2.4GHz)#############################
-if [ "$2" != "5GHZ" ]; then
-    # allow fallback to MCS0
-    iwpriv $1 set AutoFallBack=1
-    # decrease tx retry for decrease tx buffer free time
-    iwpriv $1 set LongRetry=3
-    iwpriv $1 set ShortRetry=3
 fi
