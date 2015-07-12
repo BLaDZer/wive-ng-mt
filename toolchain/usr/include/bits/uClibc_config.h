78d0fc926b2ed6957a69d0dac2fe8cdc56e35b5f
Evgeniy Manachkin <sfstudio@mail.ru>
2015-07-12 17:38:24 +0600

    rebuild toolchain.

--- toolchain/usr/include/bits/uClibc_config.h
+++ toolchain/usr/include/bits/uClibc_config.h
@@ -7,7 +7,7 @@
 #define __UCLIBC_SUBLEVEL__ 33
 /* Automatically generated make config: don't edit */
 /* Version: 0.9.33.2 */
-/* Wed Jul  8 17:18:45 2015 */
+/* Sun Jul 12 17:26:55 2015 */
 #undef __TARGET_alpha__
 #undef __TARGET_arm__
 #undef __TARGET_avr32__
