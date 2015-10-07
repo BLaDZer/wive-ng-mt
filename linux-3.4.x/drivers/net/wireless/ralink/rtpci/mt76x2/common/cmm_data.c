77a032c61786f122922031d5d0cece7900be994e
Evgeniy Manachkin <sfstudio@mail.ru>
2015-10-08 04:37:43 +0600

    REVERT: kernel: ralink_wifi: xxxx: always set ForceNonQoS flag for low rate pkts (break mtk pads radio).

--- linux-3.4.x/drivers/net/wireless/ralink/rtpci/mt76x2/common/cmm_data.c
+++ linux-3.4.x/drivers/net/wireless/ralink/rtpci/mt76x2/common/cmm_data.c
@@ -1756,21 +1756,23 @@ BOOLEAN RTMP_FillTxBlkInfo(RTMP_ADAPTER *pAd, TX_BLK *pTxBlk)
 			if ( ((RTMP_GET_PACKET_LOWRATE(pPacket)) 
 #ifdef UAPSD_SUPPORT
 				&& (!(pMacEntry && (pMacEntry->bAPSDFlagSPStart)))
-#endif /* UAPSD_SUPPORT */
+#endif /* UAPSD_SUPPORT */				
 				) ||
 				((pAd->OpMode == OPMODE_AP) && 
 				 (pMacEntry->MaxHTPhyMode.field.MODE == MODE_CCK) && (pMacEntry->MaxHTPhyMode.field.MCS == RATE_1))
 			)
 			{	/* Specific packet, i.e., bDHCPFrame, bEAPOLFrame, bWAIFrame, need force low rate.*/
 				pTxBlk->pTransmit = &pAd->MacTab.Content[MCAST_WCID].HTPhyMode;
+
 #ifdef WAPI_SUPPORT
 				/* 	According to WAPIA certification description, WAI packets can not
 					include QoS header */
 				if (RTMP_GET_PACKET_WAI(pTxBlk->pPacket))
 				{
 					TX_BLK_CLEAR_FLAG(pTxBlk, fTX_bWMM);
+					TX_BLK_SET_FLAG(pTxBlk, fTX_bForceNonQoS);
 				}
-#endif /* WAPI_SUPPORT */
+#endif /* WAPI_SUPPORT */		
 #ifdef DOT11_N_SUPPORT
 				/* Modify the WMM bit for ICV issue. If we have a packet with EOSP field need to set as 1, how to handle it? */
 				if (IS_HT_STA(pTxBlk->pMacEntry) &&
@@ -1778,10 +1780,11 @@ BOOLEAN RTMP_FillTxBlkInfo(RTMP_ADAPTER *pAd, TX_BLK *pTxBlk)
 					((pAd->CommonCfg.bRdg == TRUE) && CLIENT_STATUS_TEST_FLAG(pMacEntry, fCLIENT_STATUS_RDG_CAPABLE)))
 				{
 					TX_BLK_CLEAR_FLAG(pTxBlk, fTX_bWMM);
+					TX_BLK_SET_FLAG(pTxBlk, fTX_bForceNonQoS);
 				}
 #endif /* DOT11_N_SUPPORT */
-				TX_BLK_SET_FLAG(pTxBlk, fTX_bForceNonQoS);
 			}
+			
 #ifdef DOT11_N_SUPPORT
 			if ( (IS_HT_RATE(pMacEntry) == FALSE) &&
 				(CLIENT_STATUS_TEST_FLAG(pMacEntry, fCLIENT_STATUS_PIGGYBACK_CAPABLE)))
