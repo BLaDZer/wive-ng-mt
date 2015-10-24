abe20ce39abac9f63427b2b277dc62a4185ef83d
Evgeniy Manachkin <sfstudio@mail.ru>
2015-10-24 23:11:39 +0600

    lldpd: fix build in some system.

--- user/lldpd/DoConfigure.sh
+++ user/lldpd/DoConfigure.sh
@@ -16,12 +16,13 @@ fi
 
 CONFOPTS="--host=$HTARGET --target=$HTARGET --build=$HBUILD --prefix=$APROOTDIR/filesystem"
 CONFOPTS="$CONFOPTS --enable-shared --disable-static --disable-dependency-tracking"
-CONFOPTS="$CONFOPTS --with-readline=no --with-embedded-libevent=no --with-privsep=no --with-xml=no --with-json=no"
-CONFOPTS="$CONFOPTS --disable-privsep --disable-cdp --disable-libssp --disable-hardening"
+CONFOPTS="$CONFOPTS --with-sysusersdir=no --with-systemdsystemunitdir=no --with-launchddaemonsdir=no"
+CONFOPTS="$CONFOPTS --with-readline=no --with-xml=no --with-json=no"
+CONFOPTS="$CONFOPTS --disable-privsep --disable-cdp --disable-hardening"
 CONFOPTS="$CONFOPTS --enable-fdp --enable-edp --enable-lldpmed --enable-dot1 --enable-dot3"
 CONFOPTS="$CONFOPTS --with-lldpd-pid-file=/var/run/lldpd.pid --with-lldpd-ctl-socket=/var/run/lldpd.socket"
 
 ac_cv_lib_nl_nl_connect=no \
 LIBEVENT_CFLAGS="-I$FIRMROOT/lib/shared/include" \
 LIBEVENT_LIBS="-L$FIRMROOT/lib/shared/lib -levent" \
-./configure $CONFOPTS \
+./configure $CONFOPTS
