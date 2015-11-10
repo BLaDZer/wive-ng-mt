44a5966a865f1c513e2ed0929b6788057f8eefb9
Evgeniy Manachkin <sfstudio@mail.ru>
2015-11-11 01:51:37 +0600

    init.d: igmpsnoop in try wifi activate only if builded in drivers, M2U in bridge not depended of snooping in switch.

--- etc/scripts/wifi_advanced.sh
+++ etc/scripts/wifi_advanced.sh
@@ -32,7 +32,7 @@ if [ "$OperationMode" = "3" ] && [ "$1" != "ra0" ] && [ "$1" != "rai0" ] && [ "$
     iwpriv "$1" set ApCliAutoConnect=1
 fi
 ################################################MULTICAST PARAMS################################################
-if [ "$CONFIG_RT2860V2_AP_IGMP_SNOOP" != "" ] || [ "$CONFIG_MT76X2_AP_IGMP_SNOOP" != "" ]; then
+if [ "$CONFIG_RT2860V2_AP_IGMP_SNOOP" != "" ] || [ "$CONFIG_MT7610_AP_IGMP_SNOOP" != "" ] || [ "$CONFIG_MT76X2_AP_IGMP_SNOOP" != "" ]; then
     # in bridged mode direct enable Multicast2Unicast in wifi drivers if enabled
     # in others modes auto enable by igmpproxy
     if [ "$OperationMode" = "0" -o "$OperationMode" = "3" ]; then
