8da8bf8a3743e97905a188bb948bf620ac27d5fa
Evgeniy Manachkin <sfstudio@mail.ru>
2015-12-02 20:48:27 +0600

    xupnpd: add convex feed.

--- user/xupnpd/src/config/feeds.lua
+++ user/xupnpd/src/config/feeds.lua
@@ -1,5 +1,6 @@
 feeds=
 {
    { "generic", "http://iptv.pantyushin.ru/oms.m3u", "TV-TTK-OMSK" },
+   { "generic", "http://tv.convex.ru/TV_All.m3u", "TV-CONVEX-EKB" },
    { "generic", "http://iptv.denms.ru/iptv-rt0.m3u", "TV-ONLINE" },
 }
