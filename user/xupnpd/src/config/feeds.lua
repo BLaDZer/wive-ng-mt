597d1e4011d67121614ee63cee70b081973dc3b9
Evgeniy Manachkin <sfstudio@mail.ru>
2015-12-05 00:05:34 +0600

    cleanup feeds.

--- user/xupnpd/src/config/feeds.lua
+++ user/xupnpd/src/config/feeds.lua
@@ -3,5 +3,4 @@ feeds=
    { "generic", "http://iptv.pantyushin.ru/oms.m3u", "TV-TTK-OMSK" },
    { "generic", "http://tv.convex.ru/TV_All.m3u", "TV-CONVEX-EKB" },
    { "generic", "http://profintel.ru/files/tv/channels.m3u", "TV-INSIS-EKB" },
-   { "generic", "http://iptv.denms.ru/iptv-rt0.m3u", "TV-ONLINE" },
 }
