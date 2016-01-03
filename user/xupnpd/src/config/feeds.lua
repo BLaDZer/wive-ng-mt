0b98cb601114d7de2955df819e4979bfbf39eb48
Evgeniy Manachkin <sfstudio@mail.ru>
2016-01-03 16:03:23 +0600

    Rebuild - fix feeds for convex/insis (xupnpd not support redirects).

--- user/xupnpd/src/config/feeds.lua
+++ user/xupnpd/src/config/feeds.lua
@@ -2,6 +2,6 @@ feeds=
 {
    { "generic", "http://iptv.pantyushin.ru/oms.m3u", "TV-TTK-OMSK" },
    { "generic", "http://www.adslclub.ru/tv/ws-omsk.m3u", "TV-RTK-OMSK" },
-   { "generic", "http://tv.convex.ru/TV_All.m3u", "TV-CONVEX-EKB" },
-   { "generic", "http://profintel.ru/files/tv/channels.m3u", "TV-INSIS-EKB" },
+   { "generic", "http://www.profintel.ru/files/tv/channels.m3u", "TV-INSIS-EKB" },
+   { "generic", "http://tv.convex.ru/tv_all.m3u", "TV-CONVEX-EKB" },
 }
