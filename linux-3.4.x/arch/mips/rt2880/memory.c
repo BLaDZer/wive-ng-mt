c816efdb0777e3402eaf6c1cd085a94df13d70cb
Evgeniy Manachkin <sfstudio@mail.ru>
2016-02-24 17:42:29 +0600

    kernel: ralink_arch: mt7621: set cache_shift to 6 (workaround crashes userspace with SEGV_ACCERR).

--- linux-3.4.x/arch/mips/rt2880/memory.c
+++ linux-3.4.x/arch/mips/rt2880/memory.c
@@ -168,9 +168,9 @@ void __init prom_meminit(void)
 	if (ramsize > 0x1c000000) {
 		/* 1. Normal 0..448MB */
 		add_memory_region(RAM_BASE, 0x1c000000, BOOT_MEM_RAM);
-#if defined(CONFIG_RT2880_DRAM_512M) && defined(CONFIG_HIGHMEM) && defined(CONFIG_HIGHMEM_BOOT_RESERVE)
+#if defined(CONFIG_RT2880_DRAM_512M) && defined(CONFIG_HIGHMEM)
 		/* 2. Highmem (reserved yet, need highmem patch from MTI) */
-		add_memory_region(0x20000000, (ramsize - 0x1c000000), BOOT_MEM_RAM);
+		//add_memory_region(0x20000000, (ramsize - 0x1c000000), BOOT_MEM_RAM);
 #endif
 	} else
 #endif
