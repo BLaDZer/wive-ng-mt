f83827636d0b0299dbacca264a05aa3be1dcaf36
Evgeniy Manachkin <sfstudio@mail.ru>
2016-02-27 15:40:06 +0600

    kernel: mips: smp: remove call to ipi_call_lock()/ipi_call_unlock().

--- linux-3.4.x/include/linux/smp.h
+++ linux-3.4.x/include/linux/smp.h
@@ -88,10 +88,6 @@ int smp_call_function_any(const struct cpumask *mask,
 void __init call_function_init(void);
 void generic_smp_call_function_single_interrupt(void);
 void generic_smp_call_function_interrupt(void);
-void ipi_call_lock(void);
-void ipi_call_unlock(void);
-void ipi_call_lock_irq(void);
-void ipi_call_unlock_irq(void);
 #else
 static inline void call_function_init(void) { }
 #endif
@@ -179,7 +175,6 @@ static inline int up_smp_call_function(smp_call_func_t func, void *info)
 	} while (0)
 
 static inline void smp_send_reschedule(int cpu) { }
-#define num_booting_cpus()			1
 #define smp_prepare_boot_cpu()			do {} while (0)
 #define smp_call_function_many(mask, func, info, wait) \
 			(up_smp_call_function(func, info))
