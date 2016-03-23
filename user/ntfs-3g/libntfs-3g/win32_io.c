6b36f3e323545ce718356d30c8eea8c8dcb0de62
Evgeniy Manachkin <sfstudio@mail.ru>
2016-03-23 17:45:46 +0500

    ntfs-3g: up to 2016.2.22 version.

--- user/ntfs-3g/libntfs-3g/win32_io.c
+++ user/ntfs-3g/libntfs-3g/win32_io.c
@@ -1881,6 +1881,10 @@ static int ntfs_device_win32_ioctl(struct ntfs_device *dev, int request,
 #ifdef BLKSSZGET
 	case BLKSSZGET:
 		ntfs_log_debug("BLKSSZGET detected.\n");
+		if (fd && !fd->ntdll) {
+			*(int*)argp = fd->geo_sector_size;
+			return (0);
+		} else
 		return ntfs_win32_blksszget(dev, (int *)argp);
 #endif
 #ifdef BLKBSZSET
