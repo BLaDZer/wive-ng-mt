d0a439363448c11a2347b1a0164d836044bb315d
Evgeniy Manachkin <sfstudio@mail.ru>
2016-05-10 19:48:18 +0559

    kernel: net: ethtool: issue with querying phy settings.

--- linux-3.4.x/net/core/ethtool.c
+++ linux-3.4.x/net/core/ethtool.c
@@ -323,7 +323,15 @@ static int ethtool_get_settings(struct net_device *dev, void __user *useraddr)
 
 	cmd.phy_address = ucmd.phy_address;
 
-	err = __ethtool_get_settings(dev, &cmd);
+	if (!dev->ethtool_ops->get_settings)
+		return -EOPNOTSUPP;
+
+	if (copy_from_user(&cmd, useraddr, sizeof(cmd)))
+		return -EFAULT;
+
+	cmd.cmd = ETHTOOL_GSET;
+
+	err = dev->ethtool_ops->get_settings(dev, &cmd);
 	if (err < 0)
 		return err;
 
