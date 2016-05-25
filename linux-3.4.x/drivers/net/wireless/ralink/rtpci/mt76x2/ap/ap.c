984adf0084189477f86f6034540b238b39c3c916
Evgeniy Manachkin <sfstudio@mail.ru>
2016-05-25 18:35:26 +0559

    kernel: ralink_wifi: mt7603 remove PSM workaround (need only for 76x2 3.0.X.X drivers).

--- linux-3.4.x/drivers/net/wireless/ralink/rtpci/mt76x2/ap/ap.c
+++ linux-3.4.x/drivers/net/wireless/ralink/rtpci/mt76x2/ap/ap.c
@@ -1362,8 +1362,7 @@ VOID MacTableMaintenance(RTMP_ADAPTER *pAd)
 			}
 		}
 #ifdef BAND_STEERING
-		/* workaround for DEASSOC PSM bug, do not disconnect powersaved clients. BUGGY!!! */
-		else if (pAd->ApCfg.BndStrgTable.bEnabled == TRUE  && pEntry->PsMode != PWR_SAVE)
+		else if (pAd->ApCfg.BndStrgTable.bEnabled == TRUE)
 		{
 			if (BndStrg_IsClientStay(pAd, pEntry) == FALSE)
 			{
@@ -1383,14 +1382,9 @@ VOID MacTableMaintenance(RTMP_ADAPTER *pAd)
 			    )) {
 				if (pEntry->RssiLowStaKickOutDelayCount++ > pMbss->RssiLowForStaKickOutDelay) {
 				    pEntry->RssiLowStaKickOutDelayCount = 0;
-				    /* workaround for DEASSOC PSM bug
-				     * accumulate statistic for all clients - kick only in active state
-				    */
-				    if (pEntry->PsMode != PWR_SAVE) {
-					bDisconnectSta = TRUE;
-					printk("Disonnect STA %02x:%02x:%02x:%02x:%02x:%02x , RSSI Kickout Thres[%d] at last [%d] seconds\n",
+				    bDisconnectSta = TRUE;
+				    printk("Disonnect STA %02x:%02x:%02x:%02x:%02x:%02x , RSSI Kickout Thres[%d] at last [%d] seconds\n",
 									PRINT_MAC(pEntry->Addr), pMbss->RssiLowForStaKickOut, pMbss->RssiLowForStaKickOutDelay);
-				    }
 				}
 			} else
 				pEntry->RssiLowStaKickOutDelayCount = 0;
