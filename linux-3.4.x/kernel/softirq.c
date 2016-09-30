53943d6b6a8e71b443e562983e1165d86791591f
Evgeniy Manachkin <sfstudio@mail.ru>
2016-09-30 14:23:27 +0500

    kernel: softirq: let ksoftirqd do its job

--- linux-3.4.x/kernel/softirq.c
+++ linux-3.4.x/kernel/softirq.c
@@ -74,6 +74,17 @@ static void __fastpathsys wakeup_softirqd(void)
 }
 
 /*
+ * If ksoftirqd is scheduled, we do not want to process pending softirqs
+ * right now. Let ksoftirqd handle this at its own rate, to get fairness.
+ */
+static bool ksoftirqd_running(void)
+{
+	struct task_struct *tsk = __this_cpu_read(ksoftirqd);
+
+	return tsk && (tsk->state == TASK_RUNNING);
+}
+
+/*
  * preempt_count and SOFTIRQ_OFFSET usage:
  * - preempt_count is changed by SOFTIRQ_OFFSET on entering or leaving
  *   softirq processing.
@@ -330,7 +341,7 @@ asmlinkage void __fastpathsys do_softirq(void)
 
 	pending = local_softirq_pending();
 
-	if (pending)
+	if (pending && !ksoftirqd_running())
 		do_softirq_own_stack();
 
 	local_irq_restore(flags);
@@ -359,6 +370,9 @@ void __fastpathsys irq_enter(void)
 
 static inline void __fastpathsys invoke_softirq(void)
 {
+	if (ksoftirqd_running())
+		return;
+
 	if (!force_irqthreads) {
 		lockdep_softirq_from_hardirq();
 #ifdef CONFIG_HAVE_IRQ_EXIT_ON_IRQ_STACK
