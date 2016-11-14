9b85414ff65f56368ca0dac95171c2f008a00329
Evgeniy Manachkin <sfstudio@mail.ru>
2016-11-14 14:57:38 +0500

    revert: kernel: net: sock: fix sendmmsg for partial sendmsg.

--- linux-3.4.x/net/socket.c
+++ linux-3.4.x/net/socket.c
@@ -2096,8 +2096,6 @@ int __sys_sendmmsg(int fd, struct mmsghdr __user *mmsg, unsigned int vlen,
 		if (err)
 			break;
 		++datagrams;
-		if (msg_data_left(&msg_sys))
-			break;
 		cond_resched();
 	}
 
