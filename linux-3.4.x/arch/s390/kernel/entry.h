594680f4c5c122ec92692fcd1adc8686100fc743
Evgeniy Manachkin <sfstudio@mail.ru>
2016-12-02 12:37:00 +0500

    kernel: new helpers: current_pt_regs(), signal_pt_regs(), current_user_stack_pointer(), generic sys_fork / sys_vfork / sys_clone, mips: switch to generic sys_fork() and sys_clone(), mips: no magic arguments for sysm_pipe(), mips: sysmips: Rewrite to use SYSCALL_DEFINE3(), take sys_fork/sys_vfork/sys_clone prototypes to linux/syscalls.h (all for drop dark magic in mips).

--- linux-3.4.x/arch/s390/kernel/entry.h
+++ linux-3.4.x/arch/s390/kernel/entry.h
@@ -56,10 +56,6 @@ long sys_s390_fadvise64(int fd, u32 offset_high, u32 offset_low,
 long sys_s390_fadvise64_64(struct fadvise64_64_args __user *args);
 long sys_s390_fallocate(int fd, int mode, loff_t offset, u32 len_high,
 			u32 len_low);
-long sys_fork(void);
-long sys_clone(unsigned long newsp, unsigned long clone_flags,
-	       int __user *parent_tidptr, int __user *child_tidptr);
-long sys_vfork(void);
 void execve_tail(void);
 long sys_execve(const char __user *name, const char __user *const __user *argv,
 		const char __user *const __user *envp);
