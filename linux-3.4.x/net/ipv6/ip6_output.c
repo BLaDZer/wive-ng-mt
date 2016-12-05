a384fd14af660bcd87dbfa521d5f3d727e17741b
Evgeniy Manachkin <sfstudio@mail.ru>
2016-12-06 01:10:14 +0500

    kernel: net: ipv6: set skb->protocol on tcp, raw and ip6_append_data genereated skbs.

--- linux-3.4.x/net/ipv6/ip6_output.c
+++ linux-3.4.x/net/ipv6/ip6_output.c
@@ -253,6 +253,7 @@ int ip6_xmit(struct sock *sk, struct sk_buff *skb, struct flowi6 *fl6,
 	hdr->saddr = fl6->saddr;
 	hdr->daddr = *first_hop;
 
+	skb->protocol = htons(ETH_P_IPV6);
 	skb->priority = sk->sk_priority;
 	skb->mark = sk->sk_mark;
 
@@ -1157,6 +1158,7 @@ static inline int ip6_ufo_append_data(struct sock *sk,
 		/* initialize protocol header pointer */
 		skb->transport_header = skb->network_header + fragheaderlen;
 
+		skb->protocol = htons(ETH_P_IPV6);
 		skb->csum = 0;
 
 		__skb_queue_tail(&sk->sk_write_queue, skb);
@@ -1457,6 +1459,7 @@ alloc_new_skb:
 			/*
 			 *	Fill in the control structures
 			 */
+			skb->protocol = htons(ETH_P_IPV6);
 			skb->ip_summed = csummode;
 			skb->csum = 0;
 			/* reserve for fragmentation and ipsec header */
