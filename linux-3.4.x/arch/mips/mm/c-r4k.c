2f02b21e21e42e19db6ff9978831369568cb481c
Evgeniy Manachkin <sfstudio@mail.ru>
2016-12-23 17:38:41 +0500

    fix uncorrect apply kernel: mips: c-r4k: treat physically indexed dcaches as not aliasing

--- linux-3.4.x/arch/mips/mm/c-r4k.c
+++ linux-3.4.x/arch/mips/mm/c-r4k.c
@@ -391,10 +391,6 @@ static inline void local_r4k___flush_cache_all(void * args)
 	r4k_blast_dcache();
 	r4k_blast_icache();
 
-	/* Physically indexed caches don't suffer from virtual aliasing */
-	if (c->dcache.flags & MIPS_CACHE_PINDEX)
-		c->dcache.flags &= ~MIPS_CACHE_ALIASES;
-
 	switch (current_cpu_type()) {
 	case CPU_R4000SC:
 	case CPU_R4000MC:
@@ -1260,6 +1256,10 @@ static void __cpuinit probe_pcache(void)
 		if (c->dcache.waysize > PAGE_SIZE)
 			c->dcache.flags |= MIPS_CACHE_ALIASES;
 	}
+
+	/* Physically indexed caches don't suffer from virtual aliasing */
+	if (c->dcache.flags & MIPS_CACHE_PINDEX)
+		c->dcache.flags &= ~MIPS_CACHE_ALIASES;
 
 	switch (c->cputype) {
 	case CPU_20KC:
