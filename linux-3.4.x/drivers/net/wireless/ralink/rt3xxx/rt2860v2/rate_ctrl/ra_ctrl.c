8c00c786dfdcad42ce428e891ee9995e1eb384b3
Evgeniy Manachkin <sfstudio@mail.ru>
2017-01-20 14:54:19 +0500

    kernel: ralink_wifi: mt7620: fix legacy only B mode support

--- linux-3.4.x/drivers/net/wireless/ralink/rt3xxx/rt2860v2/rate_ctrl/ra_ctrl.c
+++ linux-3.4.x/drivers/net/wireless/ralink/rt3xxx/rt2860v2/rate_ctrl/ra_ctrl.c
@@ -662,7 +662,8 @@ VOID APMlmeSetTxRate(
 #endif /* DOT11_N_SUPPORT */
 
 	/* fix drop to CCK in 5GHz or pure OFDM modes */
-	if (tx_mode == MODE_CCK && (pAd->LatchRfRegs.Channel > 14 || (pAd->CommonCfg.PhyMode != PHY_11ABGN_MIXED && pAd->CommonCfg.PhyMode != PHY_11BGN_MIXED)))
+	if (tx_mode == MODE_CCK && (pAd->LatchRfRegs.Channel > 14 ||
+		(pAd->CommonCfg.PhyMode != PHY_11ABGN_MIXED && pAd->CommonCfg.PhyMode != PHY_11BGN_MIXED && pAd->CommonCfg.PhyMode != PHY_11B)))
 	{
 		tx_mode = MODE_OFDM;
 	}
