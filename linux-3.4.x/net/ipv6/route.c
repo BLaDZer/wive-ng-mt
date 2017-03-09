0a772a245950267a86fe5e2dc9b8b5f1f8f53df6
Evgeniy Manachkin <sfstudio@mail.ru>
2017-03-09 10:58:17 +0559

    kernel: net: route: add missing nla_policy entry for RTA_MARK attribute.

--- linux-3.4.x/net/ipv6/route.c
+++ linux-3.4.x/net/ipv6/route.c
@@ -2278,6 +2278,7 @@ static const struct nla_policy rtm_ipv6_policy[RTA_MAX+1] = {
 	[RTA_IIF]		= { .type = NLA_U32 },
 	[RTA_PRIORITY]          = { .type = NLA_U32 },
 	[RTA_METRICS]           = { .type = NLA_NESTED },
+	[RTA_MARK]		= { .type = NLA_U32 },
 };
 
 static int rtm_to_fib6_config(struct sk_buff *skb, struct nlmsghdr *nlh,
