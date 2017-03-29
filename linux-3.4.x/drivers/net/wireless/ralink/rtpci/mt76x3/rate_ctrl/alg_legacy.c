08004794f1ba8ed7ac4029490ea5941fbca0c686
Evgeniy Manachkin <sfstudio@mail.ru>
2017-03-29 13:22:10 +0459

    kernel: ralink_wifi: rate_ctl: fix fill rate table.

--- linux-3.4.x/drivers/net/wireless/ralink/rtpci/mt76x3/rate_ctrl/alg_legacy.c
+++ linux-3.4.x/drivers/net/wireless/ralink/rtpci/mt76x3/rate_ctrl/alg_legacy.c
@@ -99,6 +99,10 @@ VOID APMlmeDynamicTxRateSwitching(RTMP_ADAPTER *pAd)
 
 
 		MlmeSelectTxRateTable(pAd, pEntry, &pTable, &TableSize, &InitTxRateIdx);
+
+		if (pTable == NULL)
+			continue;
+
 		pEntry->pTable = pTable;
 
 #ifdef NEW_RATE_ADAPT_SUPPORT
@@ -500,6 +504,9 @@ VOID APQuickResponeForRateUpExec(
 			continue;
 
 		MlmeSelectTxRateTable(pAd, pEntry, &pTable, &TableSize, &InitTxRateIdx);
+
+		if (pTable == NULL)
+			continue;
 
 		pEntry->pTable = pTable;
 
