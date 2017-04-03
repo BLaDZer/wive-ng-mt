86ae22f78930862b57fba98586463e7c83225a66
root <root@sfstudio.sadnet.lo>
2017-04-03 16:13:05 +0459

    ntfs-3g: update to 2017.3.23 version.

--- user/ntfs-3g/ntfsprogs/ntfsinfo.c
+++ user/ntfs-3g/ntfsprogs/ntfsinfo.c
@@ -412,16 +412,23 @@ static const char *reparse_type_name(le32 tag)
 {
 	const char *name;
 
-	if (tag == IO_REPARSE_TAG_MOUNT_POINT)
+	switch (tag) {
+	case IO_REPARSE_TAG_MOUNT_POINT :
 		name = " (mount point)";
-	else
-		if (tag == IO_REPARSE_TAG_SYMLINK)
+		break;
+	case IO_REPARSE_TAG_SYMLINK :
 			name = " (symlink)";
-		else
-			if (tag == IO_REPARSE_TAG_WOF)
+		break;
+	case IO_REPARSE_TAG_WOF :
 				name = " (Wof compressed)";
-			else
+		break;
+	case IO_REPARSE_TAG_DEDUP :
+		name = " (deduplicated)";
+		break;
+	default :
 				name = "";
+		break;
+	}
 	return (name);
 }
 
