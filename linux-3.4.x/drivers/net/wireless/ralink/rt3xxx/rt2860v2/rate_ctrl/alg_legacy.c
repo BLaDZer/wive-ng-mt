5a9bf3bf235f8263298d4088bb0c683ef128e4b7
Evgeniy Manachkin <sfstudio@mail.ru>
2017-04-06 16:46:15 +0459

    cleanup after last patch.

--- linux-3.4.x/drivers/net/wireless/ralink/rt3xxx/rt2860v2/rate_ctrl/alg_legacy.c
+++ linux-3.4.x/drivers/net/wireless/ralink/rt3xxx/rt2860v2/rate_ctrl/alg_legacy.c
@@ -328,14 +328,6 @@ VOID APMlmeDynamicTxRateSwitching(
 			}
 		}
 
-#ifdef DOT11_VHT_AC
-		if ((Rssi > -72) && (pCurrTxRate->Mode >= MODE_VHT) && pEntry->perThrdAdj == 1)
-		{
-			TrainUp = (pCurrTxRate->TrainUp + (pCurrTxRate->TrainUp >> RA_TRAINDIV));
-			TrainDown = (pCurrTxRate->TrainDown + (pCurrTxRate->TrainDown >> RA_TRAINDIV));
-		}
-		else
-#endif /*  DOT11_VHT_AC */
 #ifdef DOT11_N_SUPPORT
 		/*
 			when Rssi > -65, there is a lot of interference usually. therefore, the algorithm
@@ -1202,14 +1194,6 @@ VOID MlmeDynamicTxRateSwitching(
 
 		pCurrTxRate = PTX_RATE_SWITCH_ENTRY(pTable, CurrRateIdx);
 
-#ifdef DOT11_VHT_AC
-		if ((Rssi > -72) && (pCurrTxRate->Mode >= MODE_VHT) && pEntry->perThrdAdj == 1)
-		{
-			TrainUp = (pCurrTxRate->TrainUp + (pCurrTxRate->TrainUp >> RA_TRAINDIV));
-			TrainDown = (pCurrTxRate->TrainDown + (pCurrTxRate->TrainDown >> RA_TRAINDIV));
-		}
-		else
-#endif /*  DOT11_VHT_AC */
 #ifdef DOT11_N_SUPPORT
 
 		/*
