e9ce286530253ead2f9b225034e44016102fdc17
Evgeniy Manachkin <sfstudio@mail.ru>
2017-05-16 12:40:31 +0459

    mt7603 exclude extfifo support build, chip not support this.

--- linux-3.4.x/drivers/net/wireless/ralink/mt76x3/include/rtmp_def.h
+++ linux-3.4.x/drivers/net/wireless/ralink/mt76x3/include/rtmp_def.h
@@ -1707,14 +1707,9 @@ enum WIFI_MODE{
 #define RA_RATE		5					/* RA every fifth 100msec period */
 #define RA_INTERVAL		(RA_RATE*100)	/* RA Interval in msec */
 
-#ifdef FIFO_EXT_SUPPORT
 /* Rate Adaptation simpling interval setting */
 #define DEF_QUICK_RA_TIME_INTERVAL		100 /* Quick RA 100 msec after rate change */
 #define DEF_RA_TIME_INTRVAL			500
-#else
-#define DEF_QUICK_RA_TIME_INTERVAL		70 /* Quick RA 70 msec after rate change */
-#define DEF_RA_TIME_INTRVAL			160
-#endif
 #define FASTRATEUPERRTH                         20 /* < 20% errors to allow fast up rate */
 
 /*definition of DRS */
