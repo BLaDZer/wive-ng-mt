a18c65f7f01e17e79693b002a342484a89443a2f
Evgeniy Manachkin <sfstudio@mail.ru>
2017-07-19 13:46:27 +0459

    kernel: net: l2tp: fix race in l2tp_recv_common().

--- linux-3.4.x/net/l2tp/l2tp_core.h
+++ linux-3.4.x/net/l2tp/l2tp_core.h
@@ -222,6 +222,9 @@ out:
 	return tunnel;
 }
 
+struct l2tp_session *l2tp_session_get(struct net *net,
+				      struct l2tp_tunnel *tunnel,
+				      u32 session_id, bool do_ref);
 extern struct l2tp_session *l2tp_session_find(struct net *net, struct l2tp_tunnel *tunnel, u32 session_id);
 extern struct l2tp_session *l2tp_session_get_nth(struct l2tp_tunnel *tunnel, int nth,
 						 bool do_ref);
