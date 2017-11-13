934f240872d6db796300bd2bc762c040cd7a7677
Evgeniy Manachkin <sfstudio@mail.ru>
2017-11-13 14:05:06 +0500

    kernel: ralink_wifi: collect very low level probe req only for 5GHz.

--- linux-3.4.x/drivers/net/wireless/ralink/mt7610/ap/ap_sync.c
+++ linux-3.4.x/drivers/net/wireless/ralink/mt7610/ap/ap_sync.c
@@ -174,8 +174,8 @@ VOID APPeerProbeReqAction(
                                   ConvertToRssi(pAd, (CHAR)Elem->Rssi1, RSSI_1),
                                   ConvertToRssi(pAd, (CHAR)Elem->Rssi2, RSSI_2));
 #ifdef BAND_STEERING
-			/* collect probe req from long range cliens for band steering */
-			if (rssi != 0 && (rssi > BND_STRG_RSSI_LOW || rssi > pAd->ApCfg.MBSSID[apidx].ProbeRspRssiThreshold)) {
+			/* collect probe req from long range clients for band steering, for 5GHz collect all frames > -88dBm */
+			if (rssi != 0 && ((rssi > BND_STRG_RSSI_LOW && pAd->CommonCfg.Channel > 14) || rssi > pAd->ApCfg.MBSSID[apidx].ProbeRspRssiThreshold)) {
 				if (WMODE_CAP_N(PhyMode))
 					    bAllowStaConnectInHt = TRUE;
 				BND_STRG_CHECK_CONNECTION_REQ(pAd, NULL, Addr2, Elem->MsgType, Elem->Rssi0, Elem->Rssi1, Elem->Rssi2, bAllowStaConnectInHt, &bBndStrgCheck);
--- linux-3.4.x/drivers/net/wireless/ralink/mt76x3/ap/ap_sync.c
+++ linux-3.4.x/drivers/net/wireless/ralink/mt76x3/ap/ap_sync.c
@@ -130,8 +130,8 @@ DBGPRINT(RT_DEBUG_OFF, ("%s():shiang! PeerProbeReqSanity failed!\n", __FUNCTION_
                                   ConvertToRssi(pAd, &Elem->rssi_info, RSSI_IDX_1),
                                   ConvertToRssi(pAd, &Elem->rssi_info, RSSI_IDX_2));
 #ifdef BAND_STEERING
-			/* collect probe req from long range cliens for band steering */
-			if (rssi != 0 && (rssi > BND_STRG_RSSI_LOW || rssi > mbss->ProbeRspRssiThreshold)) {
+			/* collect probe req from long range clients for band steering, for 5GHz collect all frames > -88dBm */
+			if (rssi != 0 && ((rssi > BND_STRG_RSSI_LOW && pAd->CommonCfg.Channel > 14) || rssi > mbss->ProbeRspRssiThreshold)) {
 				    if (WMODE_CAP_N(wdev->PhyMode))
 						bAllowStaConnectInHt = TRUE;
 				    BND_STRG_CHECK_CONNECTION_REQ(pAd, NULL, ProbeReqParam.Addr2, Elem->MsgType, Elem->rssi_info, bAllowStaConnectInHt, &bBndStrgCheck);
