8e8ce5419a19b7b31e40a533c0b61b24b8f08ff0
Evgeniy Manachkin <sfstudio@mail.ru>
2017-11-14 17:01:43 +0500

    build_system: toolchain: headers up and rebuild.

--- toolchain/usr/include/bits/uClibc_config.h
+++ toolchain/usr/include/bits/uClibc_config.h
@@ -7,7 +7,7 @@
 #define __UCLIBC_SUBLEVEL__ 33
 /* Automatically generated make config: don't edit */
 /* Version: 0.9.33.2 */
-/* Thu Oct 19 21:12:20 2017 */
+/* Tue Nov 14 16:45:53 2017 */
 #undef __TARGET_alpha__
 #undef __TARGET_arm__
 #undef __TARGET_avr32__
