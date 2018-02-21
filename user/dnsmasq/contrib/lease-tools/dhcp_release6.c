951457945db2816500e71c33b7fdc2155130a5ae
Evgeniy Manachkin <sfstudio@mail.ru>
2018-02-21 16:09:34 +0500

    dnsmasq: update to 2.79rc1 version.

--- user/dnsmasq/contrib/lease-tools/dhcp_release6.c
+++ user/dnsmasq/contrib/lease-tools/dhcp_release6.c
@@ -46,7 +46,8 @@ typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
 
-enum DHCP6_TYPES{
+enum DHCP6_TYPES
+  {
     SOLICIT = 1,
     ADVERTISE = 2,
     REQUEST = 3,
@@ -62,7 +63,9 @@ enum DHCP6_TYPES{
     RELAY_REPL = 13
     
 };
-enum DHCP6_OPTIONS{
+
+enum DHCP6_OPTIONS
+  {
     CLIENTID = 1,
     SERVERID = 2,
     IA_NA = 3,
@@ -84,7 +87,8 @@ enum DHCP6_OPTIONS{
     RECONF_ACCEPT = 20,
 };
 
-enum DHCP6_STATUSES{
+enum DHCP6_STATUSES
+  {
     SUCCESS = 0,
     UNSPEC_FAIL = 1,
     NOADDR_AVAIL=2,
@@ -137,16 +141,16 @@ struct dhcp6_iana_option{
 struct dhcp6_packet{
     size_t len;
     char buf[2048];
-    
 } ;
 
-size_t pack_duid(const char* str, char* dst){
-    
+size_t pack_duid(const char* str, char* dst)
+{
     char* tmp = strdup(str);
     char* tmp_to_free = tmp;
     char *ptr;
     uint8_t write_pos = 0;
-    while ((ptr = strtok (tmp, ":"))) {
+  while ((ptr = strtok (tmp, ":")))
+    {
         dst[write_pos] = (uint8_t) strtol(ptr, NULL, 16);
         write_pos += 1;
         tmp = NULL;
@@ -156,7 +160,8 @@ size_t pack_duid(const char* str, char* dst){
     return write_pos;
 }
 
-struct dhcp6_option create_client_id_option(const char* duid){
+struct dhcp6_option create_client_id_option(const char* duid)
+{
     struct dhcp6_option option;
     option.type = htons(CLIENTID);
     bzero(option.value, sizeof(option.value));
@@ -164,7 +169,8 @@ struct dhcp6_option create_client_id_option(const char* duid){
     return option;
 }
 
-struct dhcp6_option create_server_id_option(const char* duid){
+struct dhcp6_option create_server_id_option(const char* duid)
+{
     struct dhcp6_option   option;
     option.type = htons(SERVERID);
     bzero(option.value, sizeof(option.value));
@@ -172,7 +178,8 @@ struct dhcp6_option create_server_id_option(const char* duid){
     return option;
 }
 
-struct dhcp6_iaaddr_option create_iaadr_option(const char* ip){
+struct dhcp6_iaaddr_option create_iaadr_option(const char* ip)
+{
     struct dhcp6_iaaddr_option result;
     result.type =htons(IAADDR);
     /* no suboptions needed here, so length is 24  */
@@ -187,9 +194,12 @@ struct dhcp6_iaaddr_option create_iaadr_option(const char* ip){
             perror("inet_pton");
         exit(EXIT_FAILURE);
     }
+
     return result;
 }
-struct dhcp6_iana_option  create_iana_option(const char * iaid, struct dhcp6_iaaddr_option  ia_addr){
+
+struct dhcp6_iana_option  create_iana_option(const char * iaid, struct dhcp6_iaaddr_option  ia_addr)
+{
     struct dhcp6_iana_option  result;
     result.type = htons(IA_NA);
     result.iaid = htonl(atoi(iaid));
@@ -200,7 +210,8 @@ struct dhcp6_iana_option  create_iana_option(const char * iaid, struct dhcp6_iaa
     return result;
 }
 
-struct dhcp6_packet create_release_packet(const char* iaid, const char* ip, const char* client_id, const char* server_id){
+struct dhcp6_packet create_release_packet(const char* iaid, const char* ip, const char* client_id, const char* server_id)
+{
     struct dhcp6_packet result;
     bzero(result.buf, sizeof(result.buf));
     /* message_type */
@@ -223,21 +234,25 @@ struct dhcp6_packet create_release_packet(const char* iaid, const char* ip, cons
     return result;
 }
 
-uint16_t parse_iana_suboption(char* buf, size_t len){
+uint16_t parse_iana_suboption(char* buf, size_t len)
+{
     size_t current_pos = 0;
     char option_value[1024];
-    while (current_pos < len) {
+  while (current_pos < len)
+    {
         uint16_t option_type, option_len;
         memcpy(&option_type,buf + current_pos, sizeof(uint16_t));
         memcpy(&option_len,buf + current_pos + sizeof(uint16_t), sizeof(uint16_t));
         option_type = ntohs(option_type);
         option_len = ntohs(option_len);
         current_pos += 2 * sizeof(uint16_t);
-        if (option_type == STATUS_CODE){
+      if (option_type == STATUS_CODE)
+	{
             uint16_t status;
             memcpy(&status, buf + current_pos, sizeof(uint16_t));
             status = ntohs(status);
-            if (status != SUCCESS){
+	  if (status != SUCCESS)
+	    {
                 memcpy(option_value, buf + current_pos + sizeof(uint16_t) , option_len - sizeof(uint16_t));
                 option_value[option_len-sizeof(uint16_t)] ='\0';
                 fprintf(stderr, "Error: %s\n", option_value);
@@ -248,63 +263,75 @@ uint16_t parse_iana_suboption(char* buf, size_t len){
     return -2;
 }
 
-int16_t parse_packet(char* buf, size_t len){
+int16_t parse_packet(char* buf, size_t len)
+{
+  int16_t ret = -1;
     uint8_t type  = buf[0];
     /*skipping tx id. you need it, uncomment following line
         uint16_t tx_id = ntohs((buf[1] <<16) + (buf[2] <<8) + buf[3]);
      */
     size_t current_pos = 4;
-    if (type != REPLY ){
+  if (type != REPLY )
         return NOT_REPLY_CODE;
-    }
+  
     char option_value[1024];
-    while (current_pos < len) {
+  while (current_pos < len)
+    {
         uint16_t option_type, option_len;
         memcpy(&option_type,buf + current_pos, sizeof(uint16_t));
         memcpy(&option_len,buf + current_pos + sizeof(uint16_t), sizeof(uint16_t));
         option_type = ntohs(option_type);
         option_len = ntohs(option_len);
         current_pos += 2 * sizeof(uint16_t);
-        if (option_type == STATUS_CODE){
+      if (option_type == STATUS_CODE)
+	{
             uint16_t status;
             memcpy(&status, buf + current_pos, sizeof(uint16_t));
             status = ntohs(status);
-            if (status != SUCCESS){
+	  if (status != SUCCESS)
+	    {
                 memcpy(option_value, buf + current_pos +sizeof(uint16_t) , option_len -sizeof(uint16_t));
                 fprintf(stderr, "Error: %d %s\n", status, option_value);
                 return status;
             }
             
+	  /* Got success status, return that if there's no specific error in an IA_NA. */
+	  ret = SUCCESS;   
         }
-        if (option_type == IA_NA ){
+
+      if (option_type == IA_NA )
+	{
             uint16_t result = parse_iana_suboption(buf + current_pos +24, option_len -24);
-            if (result){
+	  if (result)
                 return result;
             }
-        }
-        current_pos += option_len;
 
+      current_pos += option_len;
     }
-    return -1;
+
+  return ret;
 }
 
-void usage(const char* arg, FILE* stream){
+void usage(const char* arg, FILE* stream)
+{
     const char* usage_string ="--ip IPv6 --iface IFACE --server-id SERVER_ID --client-id CLIENT_ID --iaid IAID [--dry-run] | --help";
     fprintf (stream, "Usage: %s %s\n", arg, usage_string);
-    
 }
 
-int send_release_packet(const char* iface, struct dhcp6_packet* packet){
-    
+int send_release_packet(const char* iface, struct dhcp6_packet* packet)
+{
     struct sockaddr_in6 server_addr, client_addr;
     char response[1400];
     int sock = socket(PF_INET6, SOCK_DGRAM, 0);
     int i = 0;
-    if (sock < 0) {
+  if (sock < 0)
+    {
         perror("creating socket");
         return -1;
     }
-    if (setsockopt(sock, SOL_SOCKET, 25, iface, strlen(iface)) == -1)  {
+  
+    if (setsockopt(sock, SOL_SOCKET, 25, iface, strlen(iface)) == -1)
+      {
         perror("SO_BINDTODEVICE");
         close(sock);
         return -1;
@@ -320,24 +347,31 @@ int send_release_packet(const char* iface, struct dhcp6_packet* packet){
     inet_pton(AF_INET6, DHCP6_MULTICAST_ADDRESS, &server_addr.sin6_addr);
     server_addr.sin6_port = htons(DHCP6_SERVER_PORT);
     int16_t recv_size = 0;
-    for (i = 0; i < 5; i++) {
-        if (sendto(sock, packet->buf, packet->len, 0,
-               (struct sockaddr *)&server_addr,
-               sizeof(server_addr)) < 0) {
+    for (i = 0; i < 5; i++)
+      {
+        if (sendto(sock, packet->buf, packet->len, 0, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+	  {
             perror("sendto failed");
             exit(4);
         }
+	
         recv_size = recvfrom(sock, response, sizeof(response), MSG_DONTWAIT, NULL, 0);
-        if (recv_size == -1){
-            if (errno == EAGAIN){
+        if (recv_size == -1)
+	  {
+            if (errno == EAGAIN)
+	      {
                 sleep(1);
                 continue;
-            }else {
+	      }
+	    else
+	      {
                 perror("recvfrom");
             }
         }
+	
         int16_t result = parse_packet(response, recv_size);
-        if (result == NOT_REPLY_CODE){
+        if (result == NOT_REPLY_CODE)
+	  {
             sleep(1);
             continue;
         }
@@ -349,7 +383,8 @@ int send_release_packet(const char* iface, struct dhcp6_packet* packet){
 }
 
 
-int main(int argc, char *  const argv[]) {
+int main(int argc, char *  const argv[])
+{
     const char* UNINITIALIZED = "";
     const char* iface = UNINITIALIZED;
     const char* ip = UNINITIALIZED;
@@ -357,17 +392,19 @@ int main(int argc, char *  const argv[]) {
     const char* server_id = UNINITIALIZED;
     const char* iaid = UNINITIALIZED;
     int dry_run = 0;
-    while (1) {
+  while (1)
+    {
         int option_index = 0;
         int c = getopt_long(argc, argv, "a:s:c:n:i:hd", longopts, &option_index);
-        if (c == -1){
+      if (c == -1)
             break;
-        }
-        switch(c){
+        
+      switch(c)
+	{
             case 0:
-                if (longopts[option_index].flag !=0){
+	  if (longopts[option_index].flag !=0)
                     break;
-                }
+	  
                 printf ("option %s", longopts[option_index].name);
                 if (optarg)
                     printf (" with arg %s", optarg);
@@ -401,29 +438,38 @@ int main(int argc, char *  const argv[]) {
                 abort();
                 
         }
-        
     }
-    if (iaid == UNINITIALIZED){
+  
+  if (iaid == UNINITIALIZED)
+    {
         fprintf(stderr, "Missing required iaid parameter\n");
         usage(argv[0], stderr);
         return -1;
     }
-    if (server_id == UNINITIALIZED){
+  
+    if (server_id == UNINITIALIZED)
+      {
         fprintf(stderr, "Missing required server-id parameter\n");
         usage(argv[0], stderr);
         return -1;
     }
-    if (client_id == UNINITIALIZED){
+    
+    if (client_id == UNINITIALIZED)
+      {
         fprintf(stderr, "Missing required client-id parameter\n");
         usage(argv[0], stderr);
         return -1;
     }
-    if (ip == UNINITIALIZED){
+    
+    if (ip == UNINITIALIZED)
+      {
         fprintf(stderr, "Missing required ip parameter\n");
         usage(argv[0], stderr);
         return -1;
     }
-    if (iface == UNINITIALIZED){
+    
+    if (iface == UNINITIALIZED)
+      {
         fprintf(stderr, "Missing required iface parameter\n");
         usage(argv[0], stderr);
         return -1;
@@ -432,14 +478,17 @@ int main(int argc, char *  const argv[]) {
 
 
     struct dhcp6_packet packet = create_release_packet(iaid, ip, client_id, server_id);
-    if (dry_run){
+
+    if (dry_run)
+      {
         uint16_t i;
-        for(i=0;i<packet.len;i++){
+
+        for(i=0; i<packet.len; i++)
             printf("%hhx", packet.buf[i]);
-        }
+        
         printf("\n");
         return 0;
     }
-    return send_release_packet(iface, &packet);
     
+    return send_release_packet(iface, &packet);
 }
