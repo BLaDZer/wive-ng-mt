50129b473eec8c3f0e90b61f4ad594435e5a0d23
Evgeniy Manachkin <sfstudio@mail.ru>
2018-04-06 18:06:42 +0459

    kernel: net: handle NAPI_GRO_FREE_STOLEN_HEAD case also in napi_frags_finish().

--- linux-3.4.x/net/core/dev.c
+++ linux-3.4.x/net/core/dev.c
@@ -3864,6 +3864,15 @@ normal:
 }
 
 
+static void napi_skb_free_stolen_head(struct sk_buff *skb)
+{
+	skb_dst_drop(skb);
+#ifdef CONFIG_XFRM
+	secpath_reset(skb);
+#endif
+	kmem_cache_free(skbuff_head_cache, skb);
+}
+
 static gro_result_t napi_skb_finish(gro_result_t ret, struct sk_buff *skb)
 {
 	switch (ret) {
@@ -3878,7 +3887,7 @@ static gro_result_t napi_skb_finish(gro_result_t ret, struct sk_buff *skb)
 
 	case GRO_MERGED_FREE:
 		if (NAPI_GRO_CB(skb)->free == NAPI_GRO_FREE_STOLEN_HEAD)
-			kmem_cache_free(skbuff_head_cache, skb);
+			napi_skb_free_stolen_head(skb);
 		else
 			__kfree_skb(skb);
 		break;
@@ -3960,8 +3969,14 @@ static gro_result_t napi_frags_finish(struct napi_struct *napi, struct sk_buff *
 		break;
 
 	case GRO_DROP:
-	case GRO_MERGED_FREE:
 		napi_reuse_skb(napi, skb);
+		break;
+
+	case GRO_MERGED_FREE:
+		if (NAPI_GRO_CB(skb)->free == NAPI_GRO_FREE_STOLEN_HEAD)
+			napi_skb_free_stolen_head(skb);
+		else
+			napi_reuse_skb(napi, skb);
 		break;
 
 	case GRO_MERGED:
