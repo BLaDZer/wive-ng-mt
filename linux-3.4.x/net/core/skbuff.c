4ad2e62cc87c0c374cb776115bca0a6df3298371
Evgeniy Manachkin <sfstudio@mail.ru>
2018-04-11 14:59:27 +0459

    kernel: net: skbuff: initialize skb->peeked when cloning.

--- linux-3.4.x/net/core/skbuff.c
+++ linux-3.4.x/net/core/skbuff.c
@@ -690,6 +690,7 @@ static struct sk_buff *__skb_clone(struct sk_buff *n, struct sk_buff *skb)
 	C(mac_len);
 	n->hdr_len = skb->nohdr ? skb_headroom(skb) : skb->hdr_len;
 	n->cloned = 1;
+	n->peeked = 0;
 	n->nohdr = 0;
 	n->destructor = NULL;
 	C(tail);
