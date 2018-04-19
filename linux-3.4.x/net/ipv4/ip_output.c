f2d1bf7029e5deaf8496e50b73444fd4d7d94e5f
Evgeniy Manachkin <sfstudio@mail.ru>
2018-04-19 16:38:52 +0459

    kernel: net: fix one possible memleak in ip_setup_cork.

--- linux-3.4.x/net/ipv4/ip_output.c
+++ linux-3.4.x/net/ipv4/ip_output.c
@@ -1064,6 +1064,10 @@ static int ip_setup_cork(struct sock *sk, struct inet_cork *cork,
 	struct ip_options_rcu *opt;
 	struct rtable *rt;
 
+	rt = *rtp;
+	if (unlikely(!rt))
+		return -EFAULT;
+
 	/*
 	 * setup for corking.
 	 */
@@ -1079,9 +1083,7 @@ static int ip_setup_cork(struct sock *sk, struct inet_cork *cork,
 		cork->flags |= IPCORK_OPT;
 		cork->addr = ipc->addr;
 	}
-	rt = *rtp;
-	if (unlikely(!rt))
-		return -EFAULT;
+
 	/*
 	 * We steal reference to this route, caller should not release it
 	 */
