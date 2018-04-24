9bc74fb87a73ca847bc6c19d689dd0d34c465b26
Evgeniy Manachkin <sfstudio@mail.ru>
2018-04-25 04:07:44 +0459

    fix insys feed.

--- user/xupnpd/src/config/feeds.lua
+++ user/xupnpd/src/config/feeds.lua
@@ -2,6 +2,6 @@ feeds=
 {
    { "generic", "http://iptv.pantyushin.ru/oms.m3u", "TV-TTK-OMSK" },
    { "generic", "http://www.adslclub.ru/tv/ws-omsk.m3u", "TV-RTK-OMSK" },
-   { "generic", "http://www.profintel.ru/files/tv/channels.m3u", "TV-INSIS-EKB" },
+   { "generic", "https://www.profintel.ru/files/tv/channels.m3u", "TV-INSIS-EKB" },
    { "generic", "http://tv.convex.ru/tv_all.m3u", "TV-CONVEX-EKB" },
 }
