4606c10ce05bfac3c6e7cbfef9556dbb866cba1a
Evgeniy Manachkin <sfstudio@mail.ru>
2018-04-29 15:43:53 +0459

    kernel: net: llc: llc: delete timers synchronously in llc_sk_free().

--- linux-3.4.x/include/net/llc_conn.h
+++ linux-3.4.x/include/net/llc_conn.h
@@ -97,6 +97,7 @@ static __inline__ char llc_backlog_type(struct sk_buff *skb)
 
 extern struct sock *llc_sk_alloc(struct net *net, int family, gfp_t priority,
 				 struct proto *prot);
+extern void llc_sk_stop_all_timers(struct sock *sk, bool sync);
 extern void llc_sk_free(struct sock *sk);
 
 extern void llc_sk_reset(struct sock *sk);
