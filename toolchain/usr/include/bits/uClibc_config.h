72837fcd0dd1f8d7d6daa6a6553418b381550983
Evgeniy Manachkin <sfstudio@mail.ru>
2018-06-28 19:33:41 +0459

    7.4.24 Released (for fabric and public).

--- toolchain/usr/include/bits/uClibc_config.h
+++ toolchain/usr/include/bits/uClibc_config.h
@@ -7,7 +7,7 @@
 #define __UCLIBC_SUBLEVEL__ 33
 /* Automatically generated make config: don't edit */
 /* Version: 0.9.33.2 */
-/* Sun Apr 29 16:27:33 2018 */
+/* Thu Jun 28 19:22:23 2018 */
 #undef __TARGET_alpha__
 #undef __TARGET_arm__
 #undef __TARGET_avr32__
