09765c47d878107edf4dbcdc5252d43fe1638798
Evgeniy Manachkin <sfstudio@mail.ru>
2018-08-25 14:42:50 +0500

    kernel: net: propagate dev_get_valid_name return code.

--- linux-3.4.x/net/core/dev.c
+++ linux-3.4.x/net/core/dev.c
@@ -6623,7 +6623,8 @@ int dev_change_net_namespace(struct net_device *dev, struct net *net, const char
 		/* We get here if we can't use the current device name */
 		if (!pat)
 			goto out;
-		if (dev_get_valid_name(dev, pat) < 0)
+		err = dev_get_valid_name(net, dev, pat);
+		if (err < 0)
 			goto out;
 	}
 
@@ -6635,7 +6636,6 @@ int dev_change_net_namespace(struct net_device *dev, struct net *net, const char
 	dev_close(dev);
 
 	/* And unlink it from device chain */
-	err = -ENODEV;
 	unlist_netdevice(dev);
 
 	synchronize_net();
