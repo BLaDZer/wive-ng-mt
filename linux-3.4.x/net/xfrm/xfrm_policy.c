32b0431ea3c2bdf7fabe060e783a4abfdaa1c839
Evgeniy Manachkin <sfstudio@mail.ru>
2018-09-06 13:43:41 +0500

    kernel: net: xfrm: fix missing dst_release() after policy blocking lbcast and multicast.

--- linux-3.4.x/net/xfrm/xfrm_policy.c
+++ linux-3.4.x/net/xfrm/xfrm_policy.c
@@ -141,6 +141,9 @@ static inline struct dst_entry *xfrm_dst_lookup(struct xfrm_state *x, int tos,
 			memcpy(prev_daddr, daddr,  sizeof(*prev_daddr));
 	}
 
+	if (IS_ERR(dst))
+		dst_release(dst_orig);
+
 	return dst;
 }
 
