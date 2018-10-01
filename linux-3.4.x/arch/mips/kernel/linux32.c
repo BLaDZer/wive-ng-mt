c5a7d63ee31aa8458a152a81a3c02473ce63a4f3
Evgeniy Manachkin <sfstudio@mail.ru>
2018-10-01 07:26:47 +0500

    kernel: mips: remove unused sys_32_mmap2.

--- linux-3.4.x/arch/mips/kernel/linux32.c
+++ linux-3.4.x/arch/mips/kernel/linux32.c
@@ -62,16 +62,6 @@
 #define merge_64(r1, r2) ((((r2) & 0xffffffffUL) << 32) + ((r1) & 0xffffffffUL))
 #endif
 
-SYSCALL_DEFINE6(32_mmap2, unsigned long, addr, unsigned long, len,
-	unsigned long, prot, unsigned long, flags, unsigned long, fd,
-	unsigned long, pgoff)
-{
-	if (pgoff & (~PAGE_MASK >> 12))
-		return -EINVAL;
-	return sys_mmap_pgoff(addr, len, prot, flags, fd,
-			      pgoff >> (PAGE_SHIFT-12));
-}
-
 /*
  * sys_execve() executes a new program.
  */
