0d334949cfcb4c8c9d925526cd920b734d97bdeb
Evgeniy Manachkin <sfstudio@mail.ru>
2018-10-10 12:34:24 +0500

    pppd: sync some patches from new git of project (commit f6be15).

--- user/ppp/pppd/main.c
+++ user/ppp/pppd/main.c
@@ -1836,7 +1836,7 @@ update_script_environment()
 		script_env[i] = newstring;
 	    else
 		add_script_env(i, newstring);
-	} else {
+	} else if (p != NULL) {
 	    remove_script_env(i);
 	}
     }
