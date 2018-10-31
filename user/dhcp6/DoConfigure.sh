0a80de69f89afcbfc0c0a1c1c7f0e425af0dc06b
Evgeniy Manachkin <sfstudio@mail.ru>
2018-10-31 12:53:12 +0500

    build_system: dhcpv6 drop dependensys from automake missing, not need.

--- user/dhcp6/DoConfigure.sh
+++ user/dhcp6/DoConfigure.sh
@@ -22,9 +22,6 @@ if [ ! -f $APROOTDIR/configure ]; then
     aclocal
     autoconf
 fi
-if [ ! -f $APROOTDIR/Makefile.in ] || [ ! -f $APROOTDIR/missing -a ! -f $APROOTDIR/build-aux/missing ]; then
-    automake --add-missing --force-missing --copy
-fi
 
 CONFOPTS="--host=$HTARGET --target=$HTARGET --build=$HBUILD"
 CONFOPTS="$CONFOPTS --prefix=$APROOTDIR/filesystem"
