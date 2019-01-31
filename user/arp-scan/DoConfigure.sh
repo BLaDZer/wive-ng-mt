0887e9beeaeedfb440e25d198a73422652e7944f
Evgeniy Manachkin <sfstudio@mail.ru>
2019-01-31 17:41:36 +0500

    add arp-scan v 1.9.5 (only 16Mb flash devices).

---
+++ user/arp-scan/DoConfigure.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "==================CONFIGURE-ARPSCAN=============================="
+APROOTDIR=`pwd`
+
+HBUILD=`uname -m`-pc-linux-gnu
+HTARGET=mipsel-linux
+
+# prefer use bash if multishell
+if [ -e /bin/bash ]; then
+    SHELL="/bin/bash"
+else
+    SHELL="/bin/sh"
+fi
+
+export SHELL
+
+if [ ! -f $APROOTDIR/configure ]; then
+    aclocal
+    autoreconf -fi
+    autoconf
+fi
+if [ ! -f $APROOTDIR/Makefile.in ] || [ ! -f $APROOTDIR/missing -a ! -f $APROOTDIR/build-aux/missing ]; then
+    automake --add-missing --force-missing --copy
+fi
+
+CONFOPTS="--host=$HTARGET --target=$HTARGET --build=$HBUILD"
+CONFOPTS="$CONFOPTS --prefix=$APROOTDIR/filesystem"
+
+./configure $CONFOPTS
